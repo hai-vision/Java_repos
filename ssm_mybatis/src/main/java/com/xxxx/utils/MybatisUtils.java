fe4b4fdebfe4bd5b08d3206c09e631d6f0db7814
HAI <2500736523@qq.com>
2022-01-28 13:35:44 +0800

    update files

--- ssm_mybatis/src/main/java/com/xxxx/utils/MybatisUtils.java
+++ ssm_mybatis/src/main/java/com/xxxx/utils/MybatisUtils.java
@@ -1,7 +1,77 @@
-package com.xxxx.utils;/**
+package com.xxxx.utils;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
  * @author hai
  * @date 2022/1/28
  * version 1.0
+ * 1. 加载配置文件
+ * 2. 创建SqlSessionFactory
+ * 3. 创建SqlSession
+ * 4. 事务管理
+ * 5. Mapper获取
  */
- @SuppressWarnings({"all"})public class MybatisUtils {
+@SuppressWarnings({"all"})
+public class MybatisUtils {
+    private static SqlSessionFactory sqlSessionFactory = null;
+    private static final ThreadLocal<SqlSession> t1 = new ThreadLocal();
+    // 由于配置文件只需要加载一次，因此将其放在静态代码块中
+    static {
+        try {
+            InputStream resourceAsStream = Resources.getResourceAsStream("mybatis-config.xml");
+            // 构建SqlSessionFactory
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+
+        } catch (IOException e) {
+            // 将编译异常转换为运行异常抛出
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 获取 SqlSession
+     * @return
+     */
+    public static SqlSession openSqlSession () {
+        SqlSession sqlSession = t1.get();
+        if ( sqlSession == null){
+            sqlSession=sqlSessionFactory.openSession();
+            t1.set(sqlSession);
+        }
+        return sqlSession;
+    }
+
+    public static void closeSession () {
+        SqlSession sqlSession = t1.get();
+        sqlSession.close();
+    }
+
+
+    public static void commit () {
+        SqlSession sqlSession = openSqlSession();
+        sqlSession.commit();
+        closeSession();
+    }
+
+    public static void rollback () {
+        SqlSession sqlSession = openSqlSession();
+        sqlSession.rollback();
+        closeSession();
+    }
+
+    public static <T> T getMapper (Class<T> cls) {
+        SqlSession sqlSession = openSqlSession();
+        return sqlSession.getMapper(cls);
+    }
+
+
+
+
 }
--- ssm_mybatis/src/test/java/com/xxxx/TestUtils.java
+++ ssm_mybatis/src/test/java/com/xxxx/TestUtils.java
@@ -1,7 +1,32 @@
-package com.xxxx;/**
+package com.xxxx;
+
+import com.xxxx.dao.StudentDAO;
+import com.xxxx.domain.Student;
+import com.xxxx.utils.MybatisUtils;
+import org.apache.ibatis.session.SqlSession;
+
+import java.util.List;
+
+/**
  * @author hai
  * @date 2022/1/28
  * version 1.0
  */
- @SuppressWarnings({"all"})public class TestUtils {
+@SuppressWarnings({"all"})
+public class TestUtils {
+    public static void main(String[] args) {
+        // 静态代码块在类加载时被调用
+        // 类加载： 实例化对象（子类对象）、调用静态成员
+
+        // 1. 得到Mapper
+        StudentDAO mapper = MybatisUtils.getMapper(StudentDAO.class);
+        // 2. 得到数据库中信息
+        List<Student> students = mapper.queryStudents();
+        for (Student student : students ) {
+            System.out.println(student);
+        }
+
+
+
+    }
 }
